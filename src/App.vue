1d7e308458a0244746f1d770783717e8d31eed4c
leenorshn <katembovictor@gmail.com>
2021-11-03 12:01:33 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,44 @@
 <template>
-
-  <div class="pt-16 mx-auto max-w-3xl">
-    <h2 class="text-2xl font-semibold text-blue-700">{{name}}</h2>
-    <p>{{age}}</p>
-    <div class="flex items-center space-x-4">
-      <h3>{{address.city}}</h3>
-      <h3>{{address.street}}</h3>
-    </div>
-    <div 
-    class="flex items-center space-x-4 mt-2 bg-blue-600 text-white rounded-md shadow-md"
-    v-for="(p ,i) in data" :key="i">
-      <span>{{i}}</span>
-      <img class="h-20 w-20 object-cover object-center" :src="p.img" alt="">
-      <div>
-        <h3>{{p.location}}</h3>
-        <p>{{p.distance}}</p>
+  <div class="max-w-3xl mx-auto bg-gray-300 pt-20 min-h-screen px-8">
+      <div class="flex items-center space-x-4">
+          <input v-model="state.newMessage" class="px-4 py-3 flex-1 rounded-md shadow" type="text" placeholder="Entrer votre message">
+      <button @click="addMessage" class="bg-blue-600 text-white px-8 py-3 rounded-md">Ajouter</button>
       </div>
-    </div>
+      <div class="flex flex-col py-4">
+      <div
+        v-for="(p, i) in state.messages"
+        :key="i"
+        class="flex items-center space-x-4 mt-1  rounded-md bg-white shadow"
+      >
+        <div class="flex-1 flex items-center">
+          <div class="px-8 py-4  rounded-l-md text-white bg-blue-700">
+          <h2>{{ i +1}}</h2>
+        </div>
+        <h2 class="text-xl flex-1 ml-4 font-semibold text-blue-600">{{ p.message }}</h2>
+        </div>
+        <div class="pr-4">
+          <button  class="text-red-500">Effacer</button>
+        </div>
+      </div>
+      </div>
   </div>
-    
 </template>
 
 <script setup>
-import data from "./data"
-  let name="Victor"
-  let age=50
-  const isOnline=true
-  let address={
-    city:"Butembo",
-    street:"Auberge Rue"
-  }
+import {reactive} from 'vue'
+const state=reactive({
+    messages:[],
+    newMessage:''
+})
+function addMessage(){
+    state.messages.unshift({
+        id:Date.now(),
+        message:state.newMessage
+    })
+    state.newMessage=""
+}
+
+
 </script>
 
 <style>
