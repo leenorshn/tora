0e183fdee9b6660078d6496c9c3ea0f7ac959f6a
leenorshn <katembovictor@gmail.com>
2021-11-05 09:17:09 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="max-w-3xl mx-auto bg-gray-300 pt-20 min-h-screen px-8">
-      <div class="flex items-center space-x-4">
-          <input v-model="state.newMessage" class="px-4 py-3 flex-1 rounded-md shadow" type="text" placeholder="Entrer votre message">
-      <button @click="addMessage" class="bg-blue-600 text-white px-8 py-3 rounded-md">Ajouter</button>
-      </div>
-      <div class="flex flex-col py-4">
+  <div class="pt-16 mx-auto max-w-4xl px-10 bg-gray-100 min-h-screen">
+    <div class="flex space-x-4">
+      <input
+        type="text"
+        v-model="state.newMessage"
+        @keydown.enter="addMessage"
+        placeholder="Entrer le donnee"
+        class="px-4 py-2 border rounded-md shadow flex-1"
+      />
+      <button
+        @click="addMessage"
+        class="px-5 py-2 rounded-md bg-blue-600 text-white"
+      >
+        Ajouter
+      </button>
+    </div>
+    <div class="flex flex-col py-4">
       <div
         v-for="(p, i) in state.messages"
         :key="i"
@@ -17,30 +28,38 @@
         <h2 class="text-xl flex-1 ml-4 font-semibold text-blue-600">{{ p.message }}</h2>
         </div>
         <div class="pr-4">
-          <button  class="text-red-500">Effacer</button>
+          <button @click="deleteMessage(p.id)" class="text-red-500">Effacer</button>
         </div>
       </div>
-      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import {reactive} from 'vue'
-const state=reactive({
-    messages:[],
-    newMessage:''
-})
-function addMessage(){
-    state.messages.unshift({
+import { reactive } from "vue";
+
+
+  const state=  reactive({
+      messages:[],
+      newMessage:''
+    })
+  
+  
+    
+    function addMessage() {
+      state.messages.unshift({
         id:Date.now(),
         message:state.newMessage
-    })
-    state.newMessage=""
-}
+      });
+      state.newMessage=""
+    }
+    function deleteMessage(id){
+     state.messages= state.messages.filter(msg=>msg.id!==id)
+    }
 
+    
 
 </script>
 
 <style>
-
 </style>
---
+++ src/Appoldd.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="max-w-3xl mx-auto bg-gray-300 pt-20 min-h-screen px-8">
+      <div class="flex items-center space-x-4">
+          <input v-model="state.newMessage" class="px-4 py-3 flex-1 rounded-md shadow" type="text" placeholder="Entrer votre message">
+      <button @click="addMessage" class="bg-blue-600 text-white px-8 py-3 rounded-md">Ajouter</button>
+      </div>
+      <div class="flex flex-col py-4">
+      <div
+        v-for="(p, i) in state.messages"
+        :key="i"
+        class="flex items-center space-x-4 mt-1  rounded-md bg-white shadow"
+      >
+        <div class="flex-1 flex items-center">
+          <div class="px-8 py-4  rounded-l-md text-white bg-blue-700">
+          <h2>{{ i +1}}</h2>
+        </div>
+        <h2 class="text-xl flex-1 ml-4 font-semibold text-blue-600">{{ p.message }}</h2>
+        </div>
+        <div class="pr-4">
+          <button  class="text-red-500">Effacer</button>
+        </div>
+      </div>
+      </div>
+  </div>
+</template>
+
+<script setup>
+import {reactive} from 'vue'
+const state=reactive({
+    messages:[],
+    newMessage:''
+})
+function addMessage(){
+    state.messages.unshift({
+        id:Date.now(),
+        message:state.newMessage
+    })
+    state.newMessage=""
+}
+
+
+</script>
+
+<style>
+
+</style>
